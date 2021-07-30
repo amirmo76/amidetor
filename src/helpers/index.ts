dea5be03052074556cac84ee48b6e312a876a41a
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-07-30 18:52:01 +0430

    added selection helpers and cypress format menu test

---
+++ src/helpers/index.ts
@@ -0,0 +1,43 @@
+type setSelectionParam = {
+  query: string;
+  offset: number;
+};
+
+export function getTextNode(el: Element, match: string) {
+  const walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null);
+  if (!match) {
+    return walk.nextNode();
+  }
+
+  let node: Node | null;
+  while ((node = walk.nextNode())) {
+    if (node.textContent?.includes(match)) {
+      return node;
+    }
+  }
+  return null;
+}
+
+export function setSelection(
+  query: string,
+  start: setSelectionParam,
+  end: setSelectionParam
+) {
+  cy.get(query)
+    .trigger('mousedown')
+    .then(($el) => {
+      const el = $el[0];
+      const document = el.ownerDocument;
+      const range = document.createRange();
+      const anchorNode = getTextNode($el[0], start.query);
+      const focusNode = getTextNode($el[0], end.query);
+      range.selectNodeContents(el);
+      if (anchorNode) range.setStart(anchorNode, start.offset);
+      if (focusNode) range.setEnd(focusNode, end.offset);
+      document.getSelection()?.removeAllRanges();
+      document.getSelection()?.addRange(range);
+    })
+    .trigger('mouseup');
+  cy.document().trigger('selectionchange');
+  return cy.get(query);
+}
