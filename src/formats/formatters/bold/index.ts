1a13aaafc7fb1334e9e0579dbd2bc3de3b8b0b4f
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-03 14:17:01 +0430

    added barrel exporting to the bold and italic formatters

---
+++ src/formats/formatters/bold/index.ts
@@ -0,0 +1,7 @@
+import Bold, { KEY } from './bold.component';
+import { Formatter } from '../../formatters.types';
+
+export default {
+  Component: Bold,
+  KEY,
+} as Formatter;
---
+++ src/formats/formatters/italic/index.ts
@@ -0,0 +1,7 @@
+import Italic, { KEY } from './italic.component';
+import { Formatter } from '../../formatters.types';
+
+export default {
+  Component: Italic,
+  KEY,
+} as Formatter;
