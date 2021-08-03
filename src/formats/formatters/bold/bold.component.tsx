6c42767de0c5e4726391a3386f5d0d38d68daed1
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-03 13:59:04 +0430

    updated bold active detection to useTestFormat hook

--- src/formats/formatters/bold/bold.component.tsx
+++ src/formats/formatters/bold/bold.component.tsx
@@ -2,22 +2,16 @@ import React, { useMemo } from 'react';
 import Button from '../../../button';
 import Icon from './bold.icon';
 import { FormatterProps } from '../../formatters.types';
-import { updateBlock, refactorChildren } from '../../formatters.utils';
+import {
+  updateBlock,
+  refactorChildren,
+  useTestFormat,
+} from '../../formatters.utils';
 
 export const KEY = 'bold';
 
 function Bold({ value, onChange, selectionInfo }: FormatterProps) {
-  const active: boolean = useMemo(() => {
-    if (!selectionInfo) return false;
-    const inBetweenParts = value.children.filter(
-      (_, i) => i >= selectionInfo.startIndex && i <= selectionInfo.endIndex
-    );
-    const hasTheFormatCount = inBetweenParts.reduce(
-      (sum, cur) => (cur[KEY] ? sum + 1 : sum),
-      0
-    );
-    return inBetweenParts.length === hasTheFormatCount ? true : false;
-  }, [value, selectionInfo]);
+  const active = useTestFormat(value, (child) => !!child[KEY], selectionInfo);
 
   const clickHandler = () => {
     if (!selectionInfo) return;
