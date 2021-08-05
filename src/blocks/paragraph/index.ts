ac54b194b29522462ef36ef9014f8599ce18a6fd
Amir Mohseni <amir.mohseni7697@gmail.com>
2021-08-05 17:27:53 +0430

    added pargraph icon to the paragraph

--- src/blocks/paragraph/index.ts
+++ src/blocks/paragraph/index.ts
@@ -1,3 +1,4 @@
 export { ParagraphBlock, ParagraphChild } from './paragraph.types';
 export const TYPE = 'paragraph';
 export { default, getEmptyBlock } from './paragraph.component';
+export * as Icons from './paragraph.icons';
--- src/blocks/paragraph/paragraph.icons.tsx
+++ src/blocks/paragraph/paragraph.icons.tsx
@@ -59,3 +59,15 @@ export const TextAlignCenterIcon = () => (
     <path d="M3,3H21V5H3V3M7,7H17V9H7V7M3,11H21V13H3V11M7,15H17V17H7V15M3,19H21V21H3V19Z" />
   </svg>
 );
+
+export const ParagraphIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M3,3H21V5H3V3M3,7H21V9H3V7M3,11H21V13H3V11M3,15H21V17H3V15M3,19H21V21H3V19Z" />
+  </svg>
+);
